f36c7aa9027b0e6f454a8ed42dc2c1a3d175c360
Sumit-Thakur10 <thakursumitmay10@gmail.com>
2024-06-10 19:04:18 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b721fdc0-2d21-47ec-91cf-bbdeb318844a';
+export const MODAL_CLOSE_SEC = 2.5;
